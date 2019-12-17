fabe60868ecb89bb45e51741c227c29b281371e1
Yuming Liang <liangyuming@aicrobo.com>
2019-12-17 15:04:45 +0800

    Change code format in CFrelement2DGridSet.cpp

--- Fremen2DGrid/src/CFrelement2DGridSet.cpp
+++ Fremen2DGrid/src/CFrelement2DGridSet.cpp
@@ -95,32 +95,50 @@ int CFrelement2DGridSet::evaluate(const char *name, uint32_t time, nav_msgs::Occ
 
 int CFrelement2DGridSet::print()
 {
-	for (int i =0;i<numGrids;i++) printf("Map %s is %ix%i\n",grids[i]->id,grids[i]->width,grids[i]->height);
+  for (int i = 0; i < numGrids; i++)
+  {
+    printf("Map %s is %ix%i\n", grids[i]->id, grids[i]->width, grids[i]->height);
+  }
 }
 
 int CFrelement2DGridSet::remove(const char *name)
 {
-	if (find(name) == false) return -numGrids;
-	delete grids[activeIndex];
-	grids[activeIndex] = grids[--numGrids];
-	return numGrids+1;
+  if (find(name) == false)
+  {
+    return -numGrids;
+  }
+
+  delete grids[activeIndex];
+
+  grids[activeIndex] = grids[--numGrids];
+
+  return numGrids + 1;
 }
 
-int CFrelement2DGridSet::save(const char *name,const char *filename)
+int CFrelement2DGridSet::save(const char *name, const char *filename)
 {
-	if (find(name) == false) return -1;
-	active->save(filename);
-	return numGrids;
+  if (find(name) == false)
+  {
+    return -1;
+  }
+
+  active->save(filename);
+
+  return numGrids;
 }
 
-int CFrelement2DGridSet::load(const char *name,const char *filename)
+int CFrelement2DGridSet::load(const char *name, const char *filename)
 {
-	bool exists = find(name);
-	if (exists == false){
-		grids[numGrids++] = new CFrelement2DGrid(name);
-		activeIndex = numGrids-1;
-		active = grids[numGrids-1];
-	}
-	active->load(filename);
-	return numGrids;
+  bool exists = find(name);
+
+  if (exists == false)
+  {
+    grids[numGrids++] = new CFrelement2DGrid(name);
+    activeIndex = numGrids - 1;
+    active = grids[numGrids - 1];
+  }
+
+  active->load(filename);
+
+  return numGrids;
 }
